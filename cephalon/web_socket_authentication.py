803589e5b7da7086213b80143da081794684138e
Toan Phung <toan.phung@proteo.info>
2023-12-22 20:11:16 +0000

    update

--- cephalon/web_socket_authentication.py
+++ cephalon/web_socket_authentication.py
@@ -28,4 +28,5 @@ class WebsocketInterchangeAPIKeyAuthenticationMiddleware:
                 api_key = await get_APIKey(api_key)
                 scope["api_key"] = api_key
                 break
+        print(scope)
         return await self.app(scope, receive, send)

b61a59508a7037e0cdd69bc45dce487b6f5a326a
Toan Phung <toan.phung@proteo.info>
2023-12-31 01:02:52 +0000

    Update consumers.py

--- corpusx/consumers.py
+++ corpusx/consumers.py
@@ -599,7 +599,6 @@ class CurrentCorpusX:
                             for t in found_terms_dict[i.id]:
                                 print(t)
                                 print(t in line)
-                                print(line.index(t))
                                 if t in line:
                                     print(rid)
                                     if rid not in found_terms_dict[i.id]:
